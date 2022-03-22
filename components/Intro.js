01a51a78a6a4686757052331507c5017c84ad027
son <son@sonui-MacBookAir.local>
2022-03-22 19:02:49 +0900

    end

--- components/Intro.js
+++ components/Intro.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import InnerPage from './InnerPage';
 import Typist from 'react-text-typist';
-import styled,{keyframes} from 'styled-components';
-// import {IoMdArrowDropdown} from 'react-icons/io';
+import styled,{keyframes,css} from 'styled-components';
+import arrow_icon from '../public/img/arrow_icon.png';
 
 
 const StyledTypist =styled(Typist)`
@@ -13,6 +13,19 @@ const StyledTypist =styled(Typist)`
         font-size: 2rem;
   }
 `;
+
+const Icons = styled.div`
+    width: 48px;
+    height: 48px;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    ${props=>props.img&&css`
+        background-image: url(${props.img});
+    `}
+    transform: rotate(90deg);
+    
+`
 
 const IntroPage = styled.div`
     padding:1rem;
@@ -63,7 +76,7 @@ function Intro({pageNum}){
                 typingSpeed={50}
                 sentences={['포기를 모르는', '오뚜기 같은 개발자', '손상일입니다.']} loop={false} />
             <UnderText>더보러 가기</UnderText>
-            {/* <UnderIcon><IoMdArrowDropdown size={36}/></UnderIcon> */}
+            <UnderIcon><Icons img={arrow_icon}/></UnderIcon>
         </IntroPage>
     )
 }
